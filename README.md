d5cc4269e4bebcb8b3631e6db8b665c750eb7f6f
Cédric Campos Carvalho <cedric.camposcarvalho@protonmail.com>
2022-05-06 09:17:14 +0200

    Added nested field with comments

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # projetWEM
+
+Create virtual environment
+```
+python -m venv venv
+```
+
+Activate the virtual environment:
+```
+venv\Scripts\activate # Windows
+source python/bin # MacOS and Linux
+```
+
+Then install all the packages in the `requirements.txt` file.
+```
+pip install -r requirements.txt
+```
+
+In the repository where the file `docker-compose.yml` is.
+
+```
+docker compose up
+```
--- spiderJVC/spiderJVC/spiders/jvc_spider.py
+++ spiderJVC/spiderJVC/spiders/jvc_spider.py
@@ -2,20 +2,35 @@ import json
 from elasticsearch_dsl.connections import connections
 import scrapy
 import scrapy_splash
-from elasticsearch_dsl import Document, Text, Float, Integer, Date
+from elasticsearch_dsl import Document, Text, Float, Integer, Date, Nested, InnerDoc
 
+
+class Comment(InnerDoc):
+    grade=Integer()
+    comment=Text()
+    date=Date()
+    username=Text()
+
+    class Index:
+        name = 'comment'
 
 class Game(Document):
     name = Text()
     editorial_grade = Integer()
     users_grade = Float()
-    release_date = Text()
+    release_date = Date()
     synopsis = Text()
     genres = Text()
     platform = Text()
 
+    #hasMany field
+    comments = Nested(Comment)
+
     class Index:
         name = 'game'
+
+    def add_comment(self, username, grade, comment, date):
+        self.comments.append(Comment(username=username,grade=grade,comment=comment,date=date))
 
     def save(self, **kwargs):
         return super(Game, self).save(**kwargs)
